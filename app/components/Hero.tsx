f4f4a04b6683e2f42e12f7fb2b480e9e89a2fda1
Satyam Upadhyay <satyamupadhyay00915@gmail.com>
2024-09-25 00:18:23 +0530

    name

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
       <div className="relative">
         <div className="text-8xl font-bold text-center">
           <h1 className="text-[#98B4CE]">Hi, I am</h1>
-          <h1 className="text-[#E48A87]">Rounit Kashyap</h1>
+          <h1 className="text-[#E48A87]">Satyam Upadhyay</h1>
         </div>
 
         <motion.div
